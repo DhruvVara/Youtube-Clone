266bebff407f85b26c1a33901d1033bb744f1129
Dhruv Vara <87010551+DhruvVara@users.noreply.github.com>
2023-12-20 17:19:39 +0530

    Delete src/store/constants.js

--- src/store/constants.js
+++
@@ -1,6 +0,0 @@
-const API_KEY="AIzaSyARReqdyifqNZ6ASagx49t7EoKblmMpElQ";
-
-export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY;
-
-export const SEARCH_API="http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
-
